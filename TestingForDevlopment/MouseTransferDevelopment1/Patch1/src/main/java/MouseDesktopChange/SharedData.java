419638ccff1572a79d29931bf07980ebbd7c1a98
Nayan Patidar <nayanpatidar28@gmail.com>
2023-09-02 15:03:21 +0530

    GUI, MouseClicks, MouseFunctionality added

--- TestingForDevlopment/MouseTransferDevelopment1/Patch1/src/main/java/MouseDesktopChange/SharedData.java
+++ TestingForDevlopment/MouseTransferDevelopment1/Patch1/src/main/java/MouseDesktopChange/SharedData.java
@@ -1,4 +1,23 @@
 package MouseDesktopChange;
 
-public class SharedData {
+public 	class SharedData {
+	// It is the shared data according to which the threads are working with
+	public static volatile int forGui = 1;
+	public static volatile int forMouseClicks;
+	public int getForGui(){
+		return forGui;
+	}
+
+	public void setForGui(int val){
+		forGui = val;
+	}
+
+	public int getForMouseClicks(){
+		forMouseClicks = forGui;
+		return forMouseClicks;
+	}
+
+//	public void setForMouseClicks(int val){
+//		this.forMouseClicks = val;
+//	}
 }
