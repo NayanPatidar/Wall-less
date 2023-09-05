d17f30f353b1200cb6c7e153876ae0f6eb138f15
Nayan Patidar <nayanpatidar28@gmail.com>
2023-09-05 18:18:42 +0530

    Removing The Changes

--- TestingForDevlopment/MouseTransferDevelopment1/Patch2/Patch2/src/main/java/Client/Main.java
+++ TestingForDevlopment/MouseTransferDevelopment1/Patch2/Patch2/src/main/java/Client/Main.java
@@ -3,9 +3,6 @@ package Client;
 public class Main {
 
 	public Main(){
-		Connection connection = new Connection();
-		if (connection.TCPConnection && connection.UDPConnection){
-			System.out.println("Connection Established");
-		}
+		ConnectionClient connection = new ConnectionClient();
 	}
 }
--- TestingForDevlopment/MouseTransferDevelopment1/Patch2/Patch2/src/main/java/Server/Main.java
+++ TestingForDevlopment/MouseTransferDevelopment1/Patch2/Patch2/src/main/java/Server/Main.java
@@ -2,39 +2,31 @@ package Server;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.*;
 
 public class Main {
+
 	static JFrame jFrame = new JFrame();
 
-	// All the sockets - UDP and TCP
 	DatagramSocket datagramSocket;
 	ServerSocket serverSocket;
 	Socket socket;
-
-	// Client Scree Size
 	String clientScreenSize;
 
-	// Ports and Address
-	int portUDP = 12345;
+	int port = 12345;
 	int portTCP = 12346;
-	InetAddress inetAddress;
-
-	// To verify if the connection is established or not
 	boolean connectionEstablished = false;
 
-	// Connections
-	boolean TCPDone = false;
-	boolean UDPDone = false;
+	InetAddress inetAddress;
 
-	public Main(){
+	public Main() {
 
-		// Creating JFrame
-		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		// Creating a JFrame
+
+		final Toolkit toolkit = Toolkit.getDefaultToolkit();
 		Dimension screenSize = toolkit.getScreenSize();
 		int screenWidth = screenSize.width;
 		int screenHeight = screenSize.height;
@@ -42,82 +34,25 @@ public class Main {
 		jFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		jFrame.setUndecorated(true);
 		jFrame.setOpacity(0.05f);
+		jFrame.setAlwaysOnTop(false);
 
-// Initialize sockets
-		try {
-			inetAddress = InetAddress.getByName("10.200.233.99");
-		} catch (UnknownHostException e) {
-			throw new RuntimeException(e);
-		}
 
+		UDPConnectionValidation();
+		TCPConnectionValidation();
+
+
+		System.out.println("Threads Started");
+//			GUIAndMouse();
 		System.out.println("ENDED");
+
 	}
 
-	private void UDPConnection() {
-		System.out.println("Here in UDPConnection");
-		try {
+	private void UDPConnectionValidation() {
 
-			// General Requirements for the connection
-			datagramSocket = new DatagramSocket(portUDP);
-			byte[] receiveData = new byte[1024];
-			String MessageToSend = "StartingUDP";
-			byte[] sendData = MessageToSend.getBytes();
-			DatagramPacket datagramPacket = new DatagramPacket(sendData, sendData.length, inetAddress, portUDP);
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-
-			System.out.println("UDP Connection starting on port: 12345");
-
-			while (true) {
-				//Sending the message to the client to verify connection
-				datagramSocket.send(datagramPacket);
-
-				//Receiving the response from the client of the verification
-				datagramSocket.receive(receivePacket);
-
-				String receivedMsg = new String(receivePacket.getData(), 0, receivePacket.getLength());
-				String[] clientMsg = receivedMsg.split("  ");
-				System.out.println(clientMsg[0]);
-				clientScreenSize = clientMsg[1];
-				if (clientMsg[0].equals("Got it")) {
-					System.out.println("Got the Message from Client: " + receivedMsg);
-					UDPDone = true;
-					break;
-				}
-			}
-		} catch (UnknownHostException | SocketException e) {
-			System.out.println("Error sending message: " + e.getMessage());
-		} catch (IOException e) {
-			System.out.println("Got IO Exception.");
-		}
 	}
 
-	private void TCPConnection() {
-		try {
+	private void TCPConnectionValidation() {
 
-			// General Requirements for the connection
-			serverSocket = new ServerSocket(portTCP);
-			socket = serverSocket.accept();
-			String sendingUDPMsg = "StartingTCP";
-			OutputStream outputStream = socket.getOutputStream();
-			byte[] sendingMsg = sendingUDPMsg.getBytes();
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			byte[] buffer = new byte[1024];
-
-			while (true) {
-				outputStream.write(sendingMsg);
-
-				String clientMessage;
-				while ((clientMessage = in.readLine()) != null) {
-					System.out.println("Received from client: " + clientMessage);
-					if (clientMessage.equals("Got it")){
-						TCPDone = true;
-						break;
-					}
-				}
-			}
-
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
 	}
+
 }
