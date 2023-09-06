12979b39b3c4511095b40c7616b4d9de364ea066
Nayan Patidar <nayanpatidar28@gmail.com>
2023-09-06 15:27:33 +0530

    Mouse+Cursor Added

--- TestingForDevlopment/MouseTransferDevelopment1/Patch2/Patch2/src/main/java/Client/Main.java
+++ TestingForDevlopment/MouseTransferDevelopment1/Patch2/Patch2/src/main/java/Client/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
 	public Main(){
 		ConnectionClient connection = new ConnectionClient();
+		if (connection.connectionEstablished){
+			System.out.println("Connection Established to the Server");
+		}
 	}
 }
