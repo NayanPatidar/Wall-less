043810a062675dd8924187f6c11ca34b060b0f6a
Nayan Patidar <nayanpatidar28@gmail.com>
2023-09-06 15:30:49 +0530

    Mouse+Cursor Added

---
+++ TestingForDevlopment/MouseTransferDevelopment1/Patch2/Patch2/src/main/java/Server/GUI.java
@@ -0,0 +1,52 @@
+package Server;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class GUI implements Runnable {
+	DatagramSocket datagramSocket;
+	ServerSocket serverSocket;
+	Socket socket;
+	JFrame jFrame;
+	SharedData sharedData;
+	InetAddress inetAddress;
+	int portUDP ;
+	int portTCP;
+	String clientScreenSize;
+
+	public GUI(JFrame jFrame, SharedData sharedData, Socket socket, InetAddress inetAddress, DatagramSocket datagramSocket, ServerSocket serverSocket, int portTCP, int portUDP, String clientScreenSize) {
+		this.socket = socket;
+		this.sharedData = sharedData;
+		this.jFrame = jFrame;
+		this.inetAddress = inetAddress;
+		this.datagramSocket = datagramSocket;
+		this.portTCP = portTCP;
+		this.portUDP = portUDP;
+		this.serverSocket = serverSocket;
+		this.clientScreenSize = clientScreenSize;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Screen Sharing started !!");
+
+		while (true){
+			Point cursor = MouseInfo.getPointerInfo().getLocation();
+
+			if (cursor.getX() < 5 && (sharedData.getForGui() == 1)){
+				System.out.println("Leaving Screen");
+				jFrame.setVisible(true);
+				sharedData.setForGui(0);
+				CoordinatesSending coordinatesSending = new CoordinatesSending(socket, datagramSocket, inetAddress, portUDP, portTCP, clientScreenSize);
+
+			} else if (cursor.getX() >= 5 && (sharedData.getForGui() == 0)) {
+				System.out.println("Entering Screen");
+				jFrame.dispose();
+				sharedData.setForGui(1);			}
+		}
+	}
+}
---
+++ TestingForDevlopment/MouseTransferDevelopment1/Patch2/Patch2/src/main/java/Server/MouseFunctionality.java
@@ -0,0 +1,35 @@
+package Server;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MouseFunctionality  {
+
+	static private JFrame jFrame;
+	private final MouseAdapter mouseAdapter;
+
+	public MouseFunctionality(JFrame frame) {
+		jFrame = frame;
+
+		mouseAdapter = new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				if (e.getButton() == MouseEvent.BUTTON1) {
+					System.out.println("Left Button");
+				} else if (e.getButton() == MouseEvent.BUTTON2) {
+					System.out.println("Middle Button");
+				} else if (e.getButton() == MouseEvent.BUTTON3) {
+					System.out.println("Right Button");
+				}
+			}
+		};
+		jFrame.addMouseListener(mouseAdapter);
+	}
+
+	public void disposeMouseListener() {
+		jFrame.removeMouseListener(mouseAdapter);
+	}
+
+}
