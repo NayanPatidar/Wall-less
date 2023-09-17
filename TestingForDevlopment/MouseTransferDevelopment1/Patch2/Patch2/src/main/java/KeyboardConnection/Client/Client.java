c3b67e865af4c4ffcf3d036282cf014dd674613f
NayanPatidar <nayanpatidar28@gmail.com>
2023-09-17 08:52:55 +0530

    KeyboardTesting Press-Release

--- TestingForDevlopment/MouseTransferDevelopment1/Patch2/Patch2/src/main/java/KeyboardConnection/Client/Client.java
+++ TestingForDevlopment/MouseTransferDevelopment1/Patch2/Patch2/src/main/java/KeyboardConnection/Client/Client.java
@@ -1,22 +1,35 @@
 package KeyboardConnection.Client;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, AWTException {
 
+        Robot robot = new Robot();
         Socket socket = new Socket("10.200.233.99", 8080);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
         byte[] buffer = new byte[1024];
-        String message = bufferedReader.readLine();
-        if (message.startsWith("K:")) {
-            String code = message.substring(2);
-            System.out.println(code);
-        }
+        while (true) {
+            String message = bufferedReader.readLine();
+            if (message.startsWith("K:")) {
+                String code = message.substring(2);
+                System.out.println(code);
 
+                switch (code){
+                    case "18":
+                        robot.keyPress(18);
+                        System.out.println("Alt Pressed");
+                        break;
+                    case "18'":
+                        robot.keyRelease(18);
+                        System.out.println("Alt Released");
+                }
+            }
+        }
     }
 }
