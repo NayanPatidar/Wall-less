3b6e8913c13df19d0ba1a9f3b1373c39ad5102e0
NayanPatidar <nayanpatidar28@gmail.com>
2023-09-17 08:45:45 +0530

    KeyboardTesting

--- TestingForDevlopment/MouseTransferDevelopment1/Patch2/Patch2/src/main/java/KeyboardConnection/Client/Client.java
+++ TestingForDevlopment/MouseTransferDevelopment1/Patch2/Patch2/src/main/java/KeyboardConnection/Client/Client.java
@@ -1,7 +1,22 @@
 package KeyboardConnection.Client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Socket socket = new Socket("10.200.233.99", 8080);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        byte[] buffer = new byte[1024];
+        String message = bufferedReader.readLine();
+        if (message.startsWith("K:")) {
+            String code = message.substring(2);
+            System.out.println(code);
+        }
 
     }
 }
