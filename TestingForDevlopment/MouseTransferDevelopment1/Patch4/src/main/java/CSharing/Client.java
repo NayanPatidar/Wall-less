0fd237ad47a2fdf920a2120345b8ab1cbf65f492
NayanPatidar <nayanpatidar28@gmail.com>
2023-10-10 00:41:31 +0530

    Minor bug Solving

--- TestingForDevlopment/MouseTransferDevelopment1/Patch4/src/main/java/CSharing/Client.java
+++ TestingForDevlopment/MouseTransferDevelopment1/Patch4/src/main/java/CSharing/Client.java
@@ -4,10 +4,7 @@ import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.*;
 
 public class Client {
     static Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
@@ -22,7 +19,8 @@ public class Client {
         }
     }
     static DatagramSocket datagramSocket;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SocketException {
+        datagramSocket = new DatagramSocket(portUDP);
         byte[] buffer = new byte[1024];
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
         try {
