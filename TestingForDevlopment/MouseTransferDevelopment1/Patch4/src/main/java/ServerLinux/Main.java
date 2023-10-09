a8bc0defd3bb2a70a7567597ee0fadda4381e500
NayanPatidar <88453419+NayanPatidar@users.noreply.github.com>
2023-10-09 22:26:53 +0530

    Update Main.java

--- TestingForDevlopment/MouseTransferDevelopment1/Patch4/src/main/java/ServerLinux/Main.java
+++ TestingForDevlopment/MouseTransferDevelopment1/Patch4/src/main/java/ServerLinux/Main.java
@@ -8,37 +8,10 @@ import java.net.UnknownHostException;
 
 public class Main {
 
-    static JFrame jFrame = new JFrame();
-    JTextField jTextField = new JTextField();
-
-    DatagramSocket datagramSocket;
-    String clientScreenSize;
-    int portUDP = 12345;
-    InetAddress inetAddress;
-    String msgFromClient = "";
-    String side = "Left";
-
-    {
-        try {
-            inetAddress = InetAddress.getByName("10.200.233.31");
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        }
-    }
     public Main () {
         System.out.println("----Starting Program----");
 
         // Creating a JFrame
-        final Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension screenSize = toolkit.getScreenSize();
-        int screenWidth = screenSize.width;
-        int screenHeight = screenSize.height;
-        jFrame.setSize(screenWidth, screenHeight);
-        jFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        jFrame.setUndecorated(true);
-        jFrame.setAlwaysOnTop(true);
-        jFrame.setBackground(new Color(0,0,0,3));
-        jFrame.setType(Window.Type.UTILITY);
 
         UDPConnectionValidation();
         System.out.println("Program Started");
