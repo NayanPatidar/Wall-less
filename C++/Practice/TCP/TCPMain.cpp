15f80effab54e29a1df096c59e18b26cfe93bfd5
NayanPatidar <nayanpatidar28@gmail.com>
2023-10-12 20:43:43 +0530

    Adding C++ Files for TCP

---
+++ C++/Practice/TCP/TCPMain.cpp
@@ -0,0 +1,72 @@
+#include <X11/Xlib.h>
+#include <iostream>
+#include <arpa/inet.h>
+#include <cstring>
+#include <unistd.h>
+
+int main(){
+
+    // Moving Cursor
+    Display* display = XOpenDisplay(NULL);
+    if (display == NULL){
+        fprintf(stderr, "Unable to open the display of Ubuntu");
+        return 1;
+    }
+
+    int x = 100;
+    int y = 200;
+
+
+
+    // Creating a socket 
+    int TCPSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (TCPSocket == -1){
+        std::cerr << "Got Error While Creating the TCP Socket "  ;  
+    }
+
+    // Using the socket functionality
+    struct sockaddr_in serverAddress, clientAddress;
+    memset(&serverAddress, 0, sizeof(serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8085);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(TCPSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0){
+        perror("Binding failed");
+        return -1;
+    }
+
+    if (listen(TCPSocket, 3) < 0) {
+        perror("Listen failed");
+        return -1;
+    }
+
+    std::cout << "Server listening on port 8085" << std::endl;
+
+     // Accept Incoming Connections
+    int clientSocket;
+    struct sockaddr_in clientAddr;
+    socklen_t addrLen = sizeof(struct sockaddr_in);
+
+    if ((clientSocket = accept(TCPSocket, (struct sockaddr *)&clientAddr, &addrLen)) < 0) {
+    perror("Accept failed");
+    return -1;
+    }
+
+     std::cout << "Connection accepted from " << inet_ntoa(clientAddr.sin_addr) << std::endl;
+
+    char buffer[1024];
+    int valread = read(clientSocket, buffer, sizeof(buffer));
+    std::cout << buffer << std::endl;
+    XWarpPointer(display, None, XRootWindow(display, XDefaultScreen(display)), 0, 0, 0, 0, x, y);
+    XFlush(display);
+    XCloseDisplay(display);
+
+    const char *response = "Hell Yeah";
+    send(clientSocket, response, strlen(response), 0);
+    std::cout << "Response Sent" << std::endl;
+
+    close(clientSocket);
+    close(TCPSocket);
+
+}
