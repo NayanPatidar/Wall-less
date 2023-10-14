e7809b8bc7ebafb49e544a396784e03a7917daa4
NayanPatidar <nayanpatidar28@gmail.com>
2023-10-15 00:31:48 +0530

    Minor Changes

--- C++/Practice/SCRN/scrn.cpp
+++ C++/Practice/SCRN/scrn.cpp
@@ -1,44 +1,53 @@
+#include <iostream>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
 int main() {
+    // Create the display
     Display *display = XOpenDisplay(NULL);
     if (!display) {
-        
+        std::cerr << "Got error in creation of display !!";
         return 1;
     }
 
+    // Create a simpleWindow 
     int screen = XDefaultScreen(display);
     Window rootWindow = XRootWindow(display, screen);
-    Window window = XCreateSimpleWindow(display, rootWindow, 0, 0, 800, 600, 0, 0, 0);
+    Window window = XCreateSimpleWindow(
+    display,
+    rootWindow,
+    0, 0,
+    800, 600,
+    0, 0,
+    0
+    );
 
-    // Allocate the grey color
+    //  Set the colour as grey
     XColor greyColor;
     Colormap colormap = XDefaultColormap(display, screen);
-    Status status = XAllocNamedColor(display, colormap, "grey", &greyColor, &greyColor);
+    Status status = XAllocNamedColor(display, colormap, "grey50", &greyColor, &greyColor);
     if (!status) {
-        
+        std::cerr << "Got error in setting the colour to grey !!";
         return 1;
     }
 
-    // Set the background color to grey
+    
+
+    // Set the window attributes 
     XSetWindowAttributes windowAttributes;
     windowAttributes.background_pixel = greyColor.pixel;
 
-    // This will remove the window manager decorations, including the title bar
     windowAttributes.override_redirect = true;
 
     XChangeWindowAttributes(display, window, CWBackPixel | CWOverrideRedirect, &windowAttributes);
 
-    // Map and manage window
+    // To make the window visible
     XMapWindow(display, window);
     XFlush(display);
 
     while (1) {
         XEvent event;
         XNextEvent(display, &event);
-
-        // Handle events as needed
     }
 
     XCloseDisplay(display);
