9ce0270ed8897c3680a915b857664d18016e70a3
Nayan Patidar <nayanpatidar28@gmail.com>
2023-10-14 11:03:02 +0530

    CRSRCoor to String

--- C++/Practice/CRSR/crsr.cpp
+++ C++/Practice/CRSR/crsr.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <Windows.h>
+#include <string.h>
 
 int main() {
 	POINT crsr;
 	while (1) {
 		if (GetCursorPos(&crsr)) {
-			std::cout << crsr.x << " " << crsr.y << std::endl;
+			//std::cout << crsr.x << " " << crsr.y << std::endl;
+			std::string coordiantes = std::to_string(crsr.x) + " " + std::to_string(crsr.y);
+			std::cout << coordiantes << std::endl;
 		}
 		Sleep(2);
 	}
+}
