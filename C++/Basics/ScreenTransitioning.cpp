d194d1692257a278dabcda58dccc09f27463d8ba
NayanPatidar <nayanpatidar28@gmail.com>
2023-10-30 21:46:20 +0530

    Added Changes to the Cursor Position

--- C++/Basics/ScreenTransitioning.cpp
+++ C++/Basics/ScreenTransitioning.cpp
@@ -2,33 +2,52 @@
 #include <X11/Xlib.h>
 #include <thread>
 
-const int MIN_X = 500;
-const int MAX_X = 600;
-const int MIN_Y = 400;
-const int MAX_Y = 500;
+const int MIN_X = 700;
+const int MAX_X = 800;
+const int MIN_Y = 350;
+const int MAX_Y = 450;
 const int POLLING_INTERVAL_MS = 10;
 const int CLIENT_WIDTH = 1280;
 const int CLIENT_HEIGHT = 720;
-int loopNumX ;
-int loopNumY ;
+int loopNumX = 1;
+int loopNumY = 1;
+int MAX_LOOPS;
 
 void moveCursor(int x, int y, Display *display) {
     XWarpPointer(display, None, XRootWindow(display, XDefaultScreen(display)), 0, 0, 0, 0, x, y);
         XFlush(display);
 }
 
+void cursorPosition(int VPofX, int VPofY){
+    int x = (VPofX-700)+(loopNumX-1)*100;
+    int x = (VPofY-350)+(loopNumY-1)*100;
+    std::cout << x << "-" << VPofY << "-" << loopNumX << std::endl;
+}
+
+
 int CalculateX(int x){
-    if(x < MIN_X){
+    if(x <= MIN_X){
+        if(loopNumX == 1){
         x = MIN_X;
+        } else if(loopNumX > 1){
+        x = MAX_X-1;
+        loopNumX--;
+        }
+        std::cout << loopNumX << std::endl;
     }
     else if (x > MAX_X){
+        if(loopNumX == MAX_LOOPS){
         x = MAX_X;
+        } else if(loopNumX < MAX_LOOPS){
+        x = MIN_X;
+        loopNumX++;
+        }
     }
     return x;
 }
 
 int CalculateY(int y){
-    if(y < MIN_Y){
+    if(y <= MIN_Y){
         y = MIN_Y;
     }
     else if (y > MAX_Y){
@@ -43,10 +62,28 @@ int main(){
         std::cerr << "Error in opening the display";
     }
 
+    if(CLIENT_WIDTH%100 == 0){
+        MAX_LOOPS = CLIENT_WIDTH/100;
+    } else {
+        MAX_LOOPS = (CLIENT_WIDTH/100) + 1;
+    }
+    std::cout << "MAX_LOOPS - " << MAX_LOOPS << std::endl;
+
+    XEvent event;
+        if (XQueryPointer(display, XRootWindow(display, DefaultScreen(display)),
+                      &event.xbutton.root, &event.xbutton.window,
+                      &event.xbutton.x_root, &event.xbutton.y_root,
+                      &event.xbutton.x, &event.xbutton.y, &event.xbutton.state)) {
+                                  
+                      int x = event.xbutton.x;
+                      int y = event.xbutton.y;
+
+                      }
+
+
 
     while (true)
     {
-    XEvent event;
     XQueryPointer(display, XRootWindow(display, DefaultScreen(display)),
                       &event.xbutton.root, &event.xbutton.window,
                       &event.xbutton.x_root, &event.xbutton.y_root,
@@ -55,13 +92,13 @@ int main(){
             // event.xbutton.x and event.xbutton.y contain the cursor coordinates
             int x = event.xbutton.x;
             int y = event.xbutton.y;    
-            std::cout << x << " " << y << std::endl;
+            // std::cout << x << " " << y << std::endl;
             int oldX,oldY;
-
-    
             
             int VPofX = CalculateX(x);
             int VPofY = CalculateY(y);
+
+            cursorPosition(VPofX, VPofY);
 
             moveCursor(VPofX, VPofY, display);
             
