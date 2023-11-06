08a116e6e359e821c220a7ef00c3510bf1e26dcc
NayanPatidar <nayanpatidar28@gmail.com>
2023-11-06 23:00:51 +0530

    String Breaking Added

---
+++ C++/Practice/DTA/One.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <regex>
+
+int main() {
+    std::string input = "KING L R L   QUEEN R   Nayan L M A";
+    std::vector<std::string> entities;
+
+    // Triple Space expression Pattern
+    std::regex pattern("\\s{3,}");
+
+    // Single Space expression Pattern
+    std::regex singleSpace("\\s{1,}");
+
+    // Use std::regex_token_iterator to split the input string
+    std::sregex_token_iterator it(input.begin(), input.end(), pattern, -1);
+    std::sregex_token_iterator end;
+
+    while (it != end) {
+        entities.push_back(*it++);
+    }
+
+    // Create a new vector to store the further split parts
+    std::vector<std::string> splitEntities;
+
+    for (const std::string& entity : entities) {
+        // Use std::regex_token_iterator to split each entity by single spaces
+        std::cout << entity << std::endl;
+        std::sregex_token_iterator two(entity.begin(), entity.end(), singleSpace, -1);
+        while (two != end) {
+            splitEntities.push_back(*two++);
+        }
+    }
+
+    std::cout << "----------------\n" << std::endl;
+
+    for (const std::string& entity : splitEntities) {
+        std::cout << entity << std::endl;
+    }
+
+    return 0;
+}
